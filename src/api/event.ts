252b7ac52a0b8449070a8b204d66782bb445751b
EricChiou <miatlab@gmail.com>
2022-04-13 22:29:45 +0800

    add event api

--- src/api/event.ts
+++ src/api/event.ts
@@ -1,5 +1,5 @@
-import { APIResponse, APIResponseStatus } from './interface/common';
-import { EventType, EventRepeatUnit, Event } from '@/types/event';
+import { APIResponse } from './interface/common';
+import { Event } from '@/types/event';
 
 import request from './base';
 
@@ -7,31 +7,21 @@ const date = new Date();
 date.setMinutes(0);
 date.setSeconds(0);
 date.setMilliseconds(0);
-const mockData: Event[] = [
-  {
-    id: 0,
-    name: '測試事件',
-    type: EventType.single,
-    startTime: date.getTime(),
-    remark: '這是備註。這是備註。這是備註。這是備註。這是備註。',
-  },
-  {
-    id: 1,
-    name: '測試事件2',
-    type: EventType.repeat,
-    repeatUnit: EventRepeatUnit.day,
-    repeatInterval: 2,
-    repeatTime: 5,
-    startTime: date.getTime() - 86400000,
-    lastTime: null,
-    remark: '這是備註。這是備註。這是備註。這是備註。這是備註。',
-  },
-];
 
 export default class EventAPI {
+  private static readonly PRE_URL = '/event';
+
   public static GetEventList(): Promise<Event[]> {
     return new Promise((resolve, reject) => {
-      request.get<APIResponse<Event[]>>('/event')
+      request.get<APIResponse<Event[]>>(this.PRE_URL)
+        .then((response) => { if (response.data.result) { resolve(response.data.result); } })
+        .catch(() => { reject(); });
+    });
+  }
+
+  public static GetEventByID(eventID: number): Promise<Event> {
+    return new Promise((resolve, reject) => {
+      request.get<APIResponse<Event>>(`/event/${eventID}`)
         .then((response) => { if (response.data.result) { resolve(response.data.result); } })
         .catch(() => { reject(); });
     });
@@ -39,7 +29,7 @@ export default class EventAPI {
 
   public static AddEvent(event: Event): Promise<APIResponse> {
     return new Promise((resolve, reject) => {
-      request.post<APIResponse>('/event/add', event)
+      request.post<APIResponse>(`${this.PRE_URL}/add`, event)
         .then((response) => { resolve(response.data); })
         .catch(() => { reject(); });
     });
@@ -47,51 +37,17 @@ export default class EventAPI {
 
   public static UpdateEvent(event: Event): Promise<APIResponse> {
     return new Promise((resolve, reject) => {
-      // mock data
-      setTimeout(() => {
-        const targetEvent = mockData.find((d) => d.id === event.id);
-        if (!targetEvent) { return; }
-        targetEvent.name = event.name;
-        targetEvent.startTime = event.startTime;
-        targetEvent.type = event.type;
-        targetEvent.remark = event.remark;
-        if (targetEvent.type === EventType.repeat && event.type === EventType.repeat) {
-          targetEvent.repeatUnit = event.repeatUnit;
-          targetEvent.repeatInterval = event.repeatInterval;
-          targetEvent.repeatTime = event.repeatTime;
-          targetEvent.lastTime = event.lastTime;
-        }
-        resolve({ status: APIResponseStatus.Success });
-      }, 100);
-      // request.put<APIResponse>(`/event/${eventID}`)
-      //   .then((response) => { resolve(response.data); })
-      //   .catch(() => { reject(); });
+      request.put<APIResponse>(this.PRE_URL, event)
+        .then((response) => { resolve(response.data); })
+        .catch(() => { reject(); });
     });
   }
 
   public static DeleteEvent(eventID: number): Promise<APIResponse> {
     return new Promise((resolve, reject) => {
-      // mock data
-      setTimeout(() => {
-        mockData.splice(mockData.findIndex((e) => e.id === eventID), 1);
-        resolve({ status: APIResponseStatus.Success });
-      }, 100);
-      // request.put<APIResponse>(`/event/${eventID}`)
-      //   .then((response) => { resolve(response.data); })
-      //   .catch(() => { reject(); });
-    });
-  }
-
-  public static GetEventByID(eventID: number): Promise<Event> {
-    return new Promise((resolve, reject) => {
-      // mock data
-      setTimeout(() => {
-        const event = mockData.find((d) => d.id === eventID);
-        if (event) { resolve(event); }
-      }, 100);
-      // request.get<Event>(`/event/${eventID}`)
-      //   .then((response) => { resolve(response.data); })
-      //   .catch(() => { reject(); });
+      request.delete<APIResponse>(`/event/${eventID}`)
+        .then((response) => { resolve(response.data); })
+        .catch(() => { reject(); });
     });
   }
 }

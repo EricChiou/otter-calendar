e6526852e81fb5886bba4e3e86a7e872c0960b5c
EricChiou <miatlab@gmail.com>
2022-04-16 16:27:03 +0800

    fix route path

--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -2,7 +2,7 @@ export default class Routes {
   public static readonly PRE_PATH = import.meta.env.PROD ? '/otter-calendar' : '';
 
   public static readonly LOGIN = `${this.PRE_PATH}/login`;
-  public static readonly CALENDAR = `${this.PRE_PATH}calendar`;
+  public static readonly CALENDAR = `${this.PRE_PATH}/calendar`;
   public static readonly EVENT_RECORDS = `${this.PRE_PATH}/event-records`;
   public static readonly SETTING = `${this.PRE_PATH}/setting`;
 }
